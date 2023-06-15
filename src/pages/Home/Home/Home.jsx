a3e345d2ff93614825183d64e9d1a8fbbb5c3b3c
Ninjax11000 <jamiljoy007@gmail.com>
2023-06-15 13:43:05 +0600

    added top slider

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-           <h2 className="text-3xl">This is home</h2> 
+           <h2 className="text-4xl font-bold text-left my-8 text-yellow-500">Unlock Your Musical Potential with Our Online School of Melodies!</h2> 
+           <Banner></Banner>
         </div>
     );
 };
