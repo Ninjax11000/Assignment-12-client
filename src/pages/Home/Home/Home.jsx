52581476e52528a6c4f2352bd433dd213f8b7897
Ninjax11000 <jamiljoy007@gmail.com>
2023-06-16 00:44:00 +0600

    added popular courses

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import PopularClass from '../PopularClass/PopularClass';
 
 const Home = () => {
     return (
         <div>
            <h2 className="text-4xl font-bold text-left my-8 text-yellow-500">Unlock Your Musical Potential with Our Online School of Melodies!</h2> 
            <Banner></Banner>
+           <PopularClass></PopularClass>
         </div>
     );
 };
